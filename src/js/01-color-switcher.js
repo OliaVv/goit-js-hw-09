3346691f8a22059f76521faa269845cf7704b398
Olha Varenyk <97734384+OliaVv@users.noreply.github.com>
2022-10-01 23:20:37 +0200

    1,2 tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+}
+function  onClick() {
+  body.style.backgroundColor = getRandomHexColor();
+    timerId = setInterval(() => {
+    changeBackgroundColor();
+    }, 1000);
+    btnStart.setAttribute('disabled', true);
+    btnStop.removeAttribute("disabled");
+    console.log("set interval");
+};
+
+btnStart.addEventListener("click", onClick);
+
+btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStop.setAttribute('disabled', true) ;
+    btnStart.removeAttribute("disabled");
+console.log("remove interval")
+})
---
+++ src/js/02-timer.js
@@ -0,0 +1,80 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const btnStart = document.querySelector('button[data-start]');
+const input = document.querySelector('input');
+const daysEl = document.querySelector("span[data-days]");
+const hoursEl = document.querySelector("span[data-hours]");
+const minutesEl = document.querySelector("span[data-minutes]");
+const secondsEl = document.querySelector("span[data-seconds]");
+//const today = Date.now();
+let intervalId = null;
+const notifyOptions = {
+    position: 'center-center',
+    backOverlay: true,
+    clickToClose: true,
+}
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] < Date.now()) {
+            Notify.failure("Оберіть дату в майбутньому :-)", notifyOptions);
+        } else {
+            btnStart.removeAttribute('disabled');
+       }
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr("#datetime-picker", options); 
+
+btnStart.addEventListener('click', timerStart);
+
+function timerStart() {
+    Notify.success("Стартуємо :-)", notifyOptions);
+    btnStart.setAttribute("disabled", true);
+    intervalId = setInterval(() => {const deadline = new Date(input.value);
+    const delta = deadline - Date.now();
+    console.log(deadline);
+    console.log(delta);
+        if (delta < 1000) {
+            clearInterval(intervalId);
+Notify.success("Час вийшов...)", notifyOptions);
+        } else {
+            showTime(convertMs(delta))
+        }
+    }, 1000)
+    
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadinZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadinZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadinZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadinZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+function addLeadinZero(value) {
+  return String(value).padStart(2, '0');
+}
+function showTime({ days, hours, minutes, seconds }) {
+
+    daysEl.textContent = days;
+    hoursEl.textContents = hours;
+    minutesEl.textContent = minutes;
+    secondsEl.textContent = seconds;
+}
